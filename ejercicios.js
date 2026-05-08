46800fa751ddfbda845ff78f95205c84bc5c4c3a
CamilaGonzalez88 <gonzalez.salazar.c92@gmail.com>
2026-05-08 17:09:04 -0400

    primer commit

--- ejercicios.js
+++ ejercicios.js
@@ -1,11 +1,9 @@
  console.log ("#ejercicio 1");
-
  let nombre = "Camila";
  let apellido = "Gonzalez";
  console.log ( nombre,apellido);
  
  console.log("#ejercicio 2");
-
  let X = 2
  let Y = 5
  let Z = 3
@@ -16,27 +14,17 @@ let comidaFavorita = "Pure con carne";
 console.log (comidaFavorita);
 
 console.log("#ejercicio 4");
-let a = 10
-let b = 3
+let a = "10"
+let b = "3"
 
-console.log("#suma");
 console.log (a + b);
-console.log("#resta");
 console.log(a - b );
-console.log("#multiplicacion");
 console.log(a * b);
-console.log("#modulo");
 console.log( a % b);
 
 console.log ("#datos cambiados a string");
 
-a = a,toString();
-b = b,toString();
 
-console.log (a + b);
-console.log(a - b );
-console.log(a * b);
-console.log( a % b);
 
 console.log ("#variable a number");
 a = "10"
@@ -47,44 +35,39 @@ console.log(a * b);
 console.log( a % b);
 
 console.log("#ejercicio 5");
-
 let edad = 15
 console.log(edad >10);
 console.log(edad >18);
 
 console.log("#ejercicio 6");
-
-let number = 8 
+let numero = 8;
+console.log( numero > 5 && numero < 10);
 
 console.log ("#ejercicio 7");
-
 let nota = 6 
 console.log(nota==7);
 
 console.log("#ejercicio 8");
-
 let temperatura = 25;
 console.log(temperatura >=30);
 
 console.log("#ejercicio 9");
-let s = 3
-let f = 4
-console.log (s ==f);
-console.log (s > f);
+let S = 3
+let F = 4
+console.log (S ==F);
+console.log (S > F);
 
 
 console.log("#ejercicio 10");
 console.log(!true);
-
 let datoedad= 17;
 let tienePermiso = true;
-console.log(edad >18 && tienePermiso);
+console.log(edad >=18 && tienePermiso);
 
 console.log("#ejercicio 11");
-
 let usuario = "admin";
 let password = "1234";
-console.log("USUARIO es", usuario,"Y password es",password);
+console.log("USUARIO es",usuario,"Y password es",password);
 
 
 
